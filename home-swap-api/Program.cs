bbec8a30dfb4595ad57ddde1a74da227d27fe7fd
alenko998 <alen_git@hotmail.com>
2024-01-22 19:11:42 +0100

    added crud for users

--- home-swap-api/Program.cs
+++ home-swap-api/Program.cs
@@ -1,4 +1,9 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using home_swap_api.Data;
+using home_swap_api.Service;
+using home_swap_api.Service.Impl;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<UserService, UserServiceImpl>();
+var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
---
+++ home-swap-api/Service/Impl/UserServiceImpl.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using home_swap_api.Data;
+using home_swap_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace home_swap_api.Service.Impl
+{
+	public class UserServiceImpl : UserService
+	{
+        private static List<User> users = new List<User>
+        {
+            new User{Id = 1, Username = "Alen" , PasswordHash = "nesto"},
+            new User{Id = 2, Username = "Second" , PasswordHash = "opala"},
+        };
+
+        private readonly AppDbContext appDbContext;
+
+        public UserServiceImpl(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<User> AddUser(User user)
+        {
+            appDbContext.Users.Add(user);
+            await appDbContext.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task<User?> DeleteUser(int id)
+        {
+            var user = await appDbContext.Users.FindAsync(id);
+            if (user is null)
+                return null;
+
+            appDbContext.Users.Remove(user);
+            await appDbContext.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            var users = await appDbContext.Users.ToListAsync();
+
+            return users;
+        }
+
+        public async Task<User?> GetSingleUser(int id)
+        {
+            var user = await appDbContext.Users.FindAsync(id);
+            if (user is null)
+                return null;
+
+            return user;
+        }
+
+        public async Task<User?> UpdateUser(int id, User user)
+        {
+            var user1 = await appDbContext.Users.FindAsync(id);
+            if (user1 is null)
+                return null;
+            user1.Username = user.Username;
+            user1.PasswordHash = user.PasswordHash;
+            await appDbContext.SaveChangesAsync();
+
+
+            return user1;
+        }
+    }
+}
+
