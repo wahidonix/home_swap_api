9cf360247f798fe7696445bcb6b55d526b7c4b04
alenko998 <alen_git@hotmail.com>
2024-02-02 11:11:48 +0100

    added login register dto

---
+++ home-swap-api/Dto/LoginRegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace home_swap_api.Dto
+{
+	public class LoginRegisterDTO
+	{
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public LoginRegisterDTO()
+		{
+		}
+	}
+}
+
