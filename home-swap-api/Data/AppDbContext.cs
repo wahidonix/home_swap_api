c610575ee95b4bc62476841587c11559f1c0bc22
alenko998 <alen_git@hotmail.com>
2024-02-06 12:58:06 +0100

    images first way

--- home-swap-api/Data/AppDbContext.cs
+++ home-swap-api/Data/AppDbContext.cs
@@ -13,6 +13,7 @@ namespace home_swap_api.Data
 		public DbSet<User> Users { get; set; }
 		public DbSet<House> Houses { get; set; }
 		public DbSet<Offer> Offers { get; set; }
+		public DbSet<Image> Images { get; set; }
     }
 }
 
--- home-swap-api/Migrations/20240206103819_ImageTable.Designer.cs
+++ home-swap-api/Migrations/20240206103819_ImageTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using home_swap_api.Data;
 
@@ -11,9 +12,11 @@ using home_swap_api.Data;
 namespace homeswapapi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240206103819_ImageTable")]
+    partial class ImageTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
